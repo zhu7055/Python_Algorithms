fba57768cde07aa3d2ccbbd90a46b92b492b8771
jiayi-zhu <118984711+zhu7055@users.noreply.github.com>
2025-07-14 19:13:34 +0800

    Add files via upload

---
+++ Sorting/Bubble_Sort.py
@@ -0,0 +1,21 @@
+def Bubble_sort(arr):
+    for n in range(len(arr)-1,0,-1):
+        #初始化swapped成false
+        swapped=False
+
+        for i in range(n):
+            if arr[i]>arr[i+1]:
+                
+                #交換元素位置，當左邊元素>右邊元素
+                arr[i],arr[i+1]=arr[i+1],arr[i]
+                #交換元素完成，標記為True
+                swapped=True
+        #如果swap未標記True，list已完成排序
+        if not swapped:
+            break
+
+arr=[12,10,9,5,7,2,50,1]
+print(f"Unsorted Array:{arr}")
+
+Bubble_sort(arr)
+print(f"Sorted list:{arr}")
