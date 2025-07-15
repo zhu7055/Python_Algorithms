58c0e2045e6c340a5365f83be9e75e3c5f5dfdc8
jiayi-zhu <118984711+zhu7055@users.noreply.github.com>
2025-07-15 16:01:47 +0800

    Update Insertion_Sort.py

--- Sorting/Insertion_Sort.py
+++ Sorting/Insertion_Sort.py
@@ -5,12 +5,13 @@ def InsertionSort(arr):
     for i in range(1,n):
         key=arr[i]#當前元素insert到正確的位置
         j=i-1
-        while j >= 0 and key < arr[j]:#在arr list範圍內
-            arr[j+1]=arr[j]#大於key位置的元素，移動
+        while j >= 0 and key < arr[j]:#移動元素，當
+            arr[j+1]=arr[j]#移動大於key位置的元素
             j-=1
         arr[j+1]=key #insertion key到正確的位置
 
-arr=[20,15,10,30,7,6,5]
-print(f"Unsorted List:{arr}")
-InsertionSort(arr)
-print(f"Sorted List:{arr}")
+if __name__=="__main__":
+    arr=[20,15,10,30,7,6,5]
+    print(f"Unsorted List:{arr}")
+    InsertionSort(arr)
+    print(f"Sorted List:{arr}")
