472bcacaf518b92c679ee9457291568aec899475
jiayi-zhu <118984711+zhu7055@users.noreply.github.com>
2025-07-18 13:40:24 +0800

    Add files via upload
    2025/7/18

--- Sorting/Bubble_Sort.py
+++ Sorting/Bubble_Sort.py
@@ -13,10 +13,10 @@ def Bubble_sort(arr):
         #如果swap未標記True，list已完成排序
         if not swapped:
             break
-
+#main function to test the bubble_sort function
 if __name__=="__main__":
     arr=[12,10,9,5,7,2,50,1]
     print(f"Unsorted list:{arr}")
 
     Bubble_sort(arr)
-    print(f"Sorted list:{arr}")
+    print(f"Sorted list(bubble_sort):{arr}")
