81fc74afce2d7efccb08f20485b2d8312f54e12c
jiayi-zhu <118984711+zhu7055@users.noreply.github.com>
2025-07-18 13:53:28 +0800

    Add files via upload

--- Searching/Binary_Search.py
+++ Searching/Binary_Search.py
@@ -9,10 +9,11 @@ def binary_search(arr:list[int],target:int,low:int,high:int)->int:
             return binary_search(arr,target,low,mid-1)
     else:
         return -1
-
+#main function to test the binary_search function
 if __name__=="__main__":
     arr=[1,7,13,15,20,50]
     target=15
+    print("Binary Search")
     result=binary_search(sorted(arr),target,0,len(arr)-1)
     if result!=-1:
         print(f"Found element at index {result}")
--- Searching/Linear_Search.py
+++ Searching/Linear_Search.py
@@ -4,10 +4,11 @@ def linear_search(arr:list[int],target:int)->int:
         if arr[i]==target:
             return i
     return -1
-
+#main function to test the linear_search function
 if __name__=="__main__":
     arr=[1,7,13,15,20,50]
     target=7
+    print("Linear Search")
     result=linear_search(arr,target)
     if result!=-1:
         print(f"Element at index {result}")
